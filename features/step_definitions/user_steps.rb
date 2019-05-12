cc30025e34d0695c773f1592c408b39ef24885b2
Melvin Hernandez <m.hernandez@berkeley.edu>
2019-05-12 02:45:43 -0700

    Attempt to fix conflict merges with master

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,7 +1,6 @@
 Given /the following users exist/ do |users_table|
   users_table.hashes.each do |user|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    # Should be part of the new profile change
     user[:ethnicity] = user[:ethnicity].split(/,/)
     users = User.create(user)
     users.save
